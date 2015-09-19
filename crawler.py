b704bf2837954439e7777c3d25620a19e651817d
Bryan <bryantsiliacos@gmail.com>
2015-09-19 14:49:39 -0700

    extract emails

--- crawler.py
+++ crawler.py
@@ -1,7 +1,13 @@
 import requests
+import re
+from bs4 import BeautifulSoup
 
 url = 'http://www.rosesluxury.com/'
 
 response = requests.get(url)
 
-print(response.text)
+soup = BeautifulSoup(response.text, "html.parser")
+
+emails = soup.body.findAll(text=re.compile("([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)", re.I))
+
+print(emails)
